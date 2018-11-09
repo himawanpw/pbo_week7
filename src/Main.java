9d4fc1b7d1348cdadaf052d4961fbcec2121c46b
Pung's Computer <wanhima47@gmail.com>
2018-11-10 01:09:59 +0700

    created by himawan

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
 import java.util.*;
 
 public class Main{
+    static List<set_get> sg1 = new ArrayList<set_get>();
+
+    public static void sortingMenu() {
+        int input = 0;
+        if (sg1.isEmpty()) {
+            System.out.println("Data masih kosong");
+        } else {
+            do {
+
+                System.out.println("");
+                System.out.println("--------------------------");
+                System.out.println("1. Sorting NIM");
+                System.out.println("2. Sorting NAMA");
+                System.out.println("3. Sorting IPK");
+                System.out.println("4. Kembali ke menu awal");
+                System.out.print("Pilih: ");
+                Scanner s = new Scanner(System.in);
+                input = s.nextInt();
+                s.nextLine();
+                switch (input) {
+                    case 1:
+                        Collections.sort(sg1, new NimComp());
+                        int counter = 1;
+                        for (set_get opt : sg1) {
+                            System.out.println("-------------------------");
+                            System.out.println("Data ke-" + counter);
+                            System.out.println("NIM : " + opt.getNim());
+                            System.out.println("Nama: " + opt.getNama());
+                            System.out.println("IPK : " + opt.getIpk());
+                            counter++;
+                        }
+                        break;
+                    case 2:
+                        Collections.sort(sg1, new NameComp());
+                        counter = 1;
+                        for (set_get opt : sg1) {
+                            System.out.println("-------------------------");
+                            System.out.println("Data ke-" + counter);
+                            System.out.println("NIM : " + opt.getNim());
+                            System.out.println("Nama: " + opt.getNama());
+                            System.out.println("IPK : " + opt.getIpk());
+                            counter++;
+                        }
+                        break;
+                    case 3:
+                        Collections.sort(sg1, new IpkComp());
+                        counter = 1;
+                        for (set_get opt : sg1) {
+                            System.out.println("-------------------------");
+                            System.out.println("Data ke-" + counter);
+                            System.out.println("NIM : " + opt.getNim());
+                            System.out.println("Nama: " + opt.getNama());
+                            System.out.println("IPK : " + opt.getIpk());
+                            counter++;
+                        }
+                        break;
+                }
+            } while (input != 4);
+        }
+    }
+
     public static void main(String args[]){
-        List<set_get> sg1 = new ArrayList<set_get>();
         int input = 0;
         do {
             System.out.println("");
@@ -42,60 +102,7 @@ public class Main{
                     }
                     break;
                 case 3:
-                    if(sg1.isEmpty()){
-                        System.out.println("Data masih kosong");
-                    }
-                    else {
-                        do {
-                            System.out.println("");
-                            System.out.println("--------------------------");
-                            System.out.println("1. Sorting NIM");
-                            System.out.println("2. Sorting NAMA");
-                            System.out.println("3. Sorting IPK");
-                            System.out.println("4. Kembali ke menu awal");
-                            System.out.print("Pilih: ");
-                            input = s.nextInt();
-                            s.nextLine();
-                            switch (input) {
-                                case 1:
-                                    Collections.sort(sg1 , new NimComp());
-                                    int counter = 1;;
-                                    for (set_get opt:sg1) {
-                                        System.out.println("-------------------------");
-                                            System.out.println("Data ke-" + counter);
-                                            System.out.println("NIM : " + opt.getNim());
-                                            System.out.println("Nama: " + opt.getNama());
-                                            System.out.println("IPK : " + opt.getIpk());
-                                            counter++;
-                                }
-                                break;
-                                case 2:
-                                    Collections.sort(sg1 , new NameComp());
-                                    counter = 1;
-                                    for (set_get opt:sg1) {
-                                        System.out.println("-------------------------");
-                                        System.out.println("Data ke-" + counter);
-                                        System.out.println("NIM : " + opt.getNim());
-                                        System.out.println("Nama: " + opt.getNama());
-                                        System.out.println("IPK : " + opt.getIpk());
-                                        counter++;
-                                    }
-                                    break;
-                                case 3:
-                                    Collections.sort(sg1 , new IpkComp());
-                                    counter = 1;
-                                    for (set_get opt:sg1) {
-                                        System.out.println("-------------------------");
-                                        System.out.println("Data ke-" + counter);
-                                        System.out.println("NIM : " + opt.getNim());
-                                        System.out.println("Nama: " + opt.getNama());
-                                        System.out.println("IPK : " + opt.getIpk());
-                                        counter++;
-                                    }
-                                    break;
-                            }
-                        } while (input != 4);
-                    }
+                    sortingMenu();
                     break;
             }
         } while (input != 4);
